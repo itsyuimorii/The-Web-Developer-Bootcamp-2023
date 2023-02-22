094b555aa1089d51ba0c267f1770d59ae0a77949
YUIMORII <yuimoriistudio@gmail.com>
2023-02-21 23:10:37 -0600

    Update campground.js

--- 🌟Final Capstone/KyanpuCamp/models/campground.js
+++ 🌟Final Capstone/KyanpuCamp/models/campground.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+mongoose.connect("mongodb://127.0.0.1:27017/KyanpuCamp", {
+  userNewUrlParser: true,
+  userCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
 const CampgroundSchema = mongoose.Schema({
   title: String,
   price: String,
