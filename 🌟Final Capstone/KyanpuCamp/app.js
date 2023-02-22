7bdc84af4d0d5e09272ddbdf06a39cf46ca3b9f2
YUIMORII <yuimoriistudio@gmail.com>
2023-02-21 22:57:36 -0600

    Update

--- 🌟Final Capstone/KyanpuCamp/app.js
+++ 🌟Final Capstone/KyanpuCamp/app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+
+const path = require("path");
 const app = express();
 
-app.listen("listening on port http://localhost:8080");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port https://localhost:3000");
+});
