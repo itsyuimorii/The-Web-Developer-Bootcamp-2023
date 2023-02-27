785112516e7af61e0e64f8bc9e34dcf940111fd8
YUIMORII <yuimoriistudio@gmail.com>
2023-02-27 14:49:43 -0600

    update

--- 🌟Final Capstone/👀 Final_code_kyanpuCamp/seeds/index.js
+++ 🌟Final Capstone/👀 Final_code_kyanpuCamp/seeds/index.js
@@ -35,7 +35,7 @@ const seedDB = async () => {
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://unsplash.com/collection/3671729",
+      image: "https://source.unsplash.com/random/?city,night",
       description:
         "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
       price,
